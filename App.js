707e7cbdd45f4378ef63d07459a5fed6916e0b35
ABDELGHAFOUR BARANE <abdelghafourbarane@gmail.com>
2021-08-25 23:45:34 +0100

    init the project using expo init , add homeScreen,listScreen,componentsScreen and ImagesScreen

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
-
+import ListScreen from "./src/screens/ListScreen";
+import Components from "./src/screens/Components";
+import ImageScreen from "./src/screens/ImageScreen";
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Home: HomeScreen,
+    List: ListScreen,
+    Components: Components,
+    Images: ImageScreen,
   },
   {
     initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "App"
-    }
+      title: "App",
+    },
   }
 );
 
---
+++ src/components/ImageDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+function ImageDetails() {
+  return <Text>Image detail</Text>;
+}
+
+const styles = StyleSheet.create({});
+export default ImageDetails;
